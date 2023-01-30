bee122200941a64d78b2607ef93435a7fac9f2ac
Steve Lening <micheltalaupa@gmail.com>
2023-01-30 15:55:57 +0100

    ajout de la branche lening

---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = (props) => {
+    return (
+        <div>
+            Ceci est le component Login
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingUp = (props) => {
+    return (
+        <div>
+            Ceci est le component SingUp
+        </div>
+    );
+};
+
+export default SingUp;
