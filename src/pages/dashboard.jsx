2342ec741a719c3c510b115726a83ef6d43c2ef6
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-01-30 15:45:42 +0100

    First commit of the project

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dashboard = () => {
+    return (
+        <div>
+            Ceci est le dashboard. OK. lmksjflkdsj
+        </div>
+    );
+};
+
+export default dashboard;
