28f6b109754aabd4a185ea75aee52bcb3cd2acb7
unknown <haterb2803@gmail.com>
2023-02-01 13:25:53 +0100

    choose place component

--- src/componnents/index.js
+++ src/componnents/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as Navbar } from './Navbar';
 export { default as SearchForm } from './SearchForm'
 export { default as SearchItem } from './SearchItem'
+export { default as PlaceSelector } from './PlaceSelector'
