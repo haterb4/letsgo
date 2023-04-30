49e0d3df006645688a185443561c4c484a7fd4d5
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-04-30 14:23:18 +0100

    Essai d'utilisation des grilles: ECHEC

--- src/componnents/dashboard/BoxNumber.jsx
+++ src/componnents/dashboard/BoxNumber.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
 const BoxNumber = ({ number, text, image, color }) => {
+    const elt = typeof(number) === 'string' && number.split(' ').length >= 2 ?  number.split(' ').map(
+        (value, index) => <span className="lines" key={index}>{value}</span>) : number
     return (
         <div className="boxNumber flex place-items-center p-4 gap-4">
-            <div className="h-36">
-                <img src={image} alt="icone" className="w-36 h-36" />
+            <div className="h-24">
+                <img src={image} alt="icone" className="w-24 h-24" />
             </div>
-            <div className="h-full">
-                <p className="uppercase text-lg">{text}</p>
-                <p style={{color: color}} className="text-5xl">{number}</p>
+            <div className="h-full pt-1">
+                <p className="text uppercase">{text}</p>
+                <p style={{color: color}} className="number text-5xl font-bold">{elt}</p>
             </div>      
         </div>
     );
--- src/componnents/dashboard/HomeCenter.jsx
+++ src/componnents/dashboard/HomeCenter.jsx
@@ -15,12 +15,12 @@ const HomeCenter = () => {
         {number: driversCount, text: 'Drivers', image: '/images/dashboard/volan.png', color: '#2D3A96'},
         {number: vehiclesCount, text: 'Vehicles', image: '/images/dashboard/voitures.png', color: '#4285F4'},
         {number: publishedTripsCount, text: 'Published trips', image: '/images/dashboard/trip_bleu.png', color: '#2D3A96'},
-        {number: completedTripsCount, text: 'Completed trips', image: '/images/dashboard/trip_vert.png', color: '#2D3A96'},
+        {number: completedTripsCount, text: 'Completed trips', image: '/images/dashboard/trip_vert.png', color: '#296E3B'},
         {number: recentGain, text: 'Recent gain', image: '/images/dashboard/gains.png', color: '#2D3A96'},
     ]
 
     return (
-    <div id="HomeCenter" className="mt-5 flex flex-wrap gap-5">
+    <div id="HomeCenter" className="mt-5">
         {
             data.map( (elt, index) => <BoxNumber key={index} number={elt.number} text={elt.text} image={elt.image} color={elt.color} />)
         }
