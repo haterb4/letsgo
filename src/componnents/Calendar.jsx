c2e2750baee751bd8dc6393efc8388551bc04440
Djoumessi-Aurel <aurelbazir@gmail.com>
2023-06-21 22:02:37 +0100

    Mise à jour de la Sidebar - Correction responsive landing page.__

--- src/componnents/Calendar.jsx
+++ src/componnents/Calendar.jsx
@@ -22,20 +22,6 @@ const Calendar = forwardRef((props, ref) => {
     );
   });
 
-// const Calendar = () => {
-//     const calendarRef = useRef()
-
-//     const open = ()=>calendarRef.current.setOpen(true)
-
-//     const [chosenDate, setChosenDate] = useState(new Date());
-//     const ExampleCustomInput = forwardRef(({ value, onClick }, ref) => (
-//         <input type="text" placeholder="Departure Date & Time" 
-//         className="border-t border-r  lg:border-t-0 border-0 text-sm lg:text-base lg:text-left text-center lg:pl-6 px-4 lg:py-2 py-4 focus:outline-0 focus:ring-0"
-//         onClick={onClick} ref={ref} value={value} readOnly
-//         />
-//       ));
-
-
-// };
+  Calendar.displayName = 'Calendar';
 
 export default Calendar;
